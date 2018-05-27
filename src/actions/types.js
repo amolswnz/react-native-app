7a7948245b35ec00aa3e74886b735012228ec9d9
Amol Wankhede <amolsw.nz@gmail.com>
2018-05-27 20:16:40 +1200

    login success fail loading

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,5 @@
 export const EMAIL_CHANGED = 'email_changed';
 export const PASSWORD_CHANGED = 'password_changed';
+export const LOGIN_USER_SUCCESS = 'login user success';
+export const LOGIN_USER_FAIL = 'login user failed';
+export const LOGIN_USER = 'login user spinner loading';
